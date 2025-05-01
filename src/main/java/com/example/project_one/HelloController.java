2c5e6b3e3cde4269e2ae1168175fb2f2b7cacc2f
Enoch Silva <enochsilva69@gmail.com>
2025-05-01 20:22:35 +0100

    change in controller

--- src/main/java/com/example/project_one/HelloController.java
+++ src/main/java/com/example/project_one/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    // Endpoint to return a greeting message
     public String hello() {
         return "Hello from Spring Boot!";
     }
