93d29ac3144c8c8b3080b3c09bfb40485b5f4f2d
Enoque da Cruz Silva <135479296+Enoch-Silva@users.noreply.github.com>
2025-05-01 20:17:47 +0100

    Update HelloController.java

--- src/main/java/com/example/project_one/HelloController.java
+++ src/main/java/com/example/project_one/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+    //This is a comment added from the github website!
     public String hello() {
         return "Hello from Spring Boot!";
     }
